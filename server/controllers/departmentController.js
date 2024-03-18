d89b55f4a9d1d42d9f9796075932a0bcec9dc143
Iman Sheyma <imansheyma1@gmail.com>
2024-03-19 02:51:06 +0300

    department add method adj

--- server/controllers/departmentController.js
+++ server/controllers/departmentController.js
@@ -1,20 +1,30 @@
 const DepartmentModel = require('../models/department');
 
+//контроллер для работы с department
 class DepartmentController{
+
+    //получение записей
     static async getAllDepartments(req, res){
-        var result = await DepartmentModel.getDepartments();
-        if(result)
-            res.send(result);
+        try{
+            var result = await DepartmentModel.getDepartments();
+            if(result)
+                res.send(result);
+        } catch (err){
+            res.status(400);
+        }
     }
 
+    //добавление записей
     static async addDepartments(req, res){
-        const departments = req.body.map(department => [department.name]);
-        
-        var result = await DepartmentModel.addDepartments(departments);
-        if(result)
-            res.send('record added successfully');
-        else
-            res.send('record creation failed');
+        try{
+            var result = await DepartmentModel.addDepartments(req.body);
+            if(result)
+                res.status(200).send('record added successfully');
+            else
+                res.status(400).send('record creation failed');
+        } catch(err){
+            res.status(400).send('record creation failed');
+        }
     }
 }
 
--- server/models/department.js
+++ server/models/department.js
@@ -1,18 +1,25 @@
 const db = require('../config/db');
 
+//класс для взаимодействия с таблицей department
 class DepartmentModel {
+
+    //запрос записей из таблицы
     static async getDepartments(){
         return new Promise(resolve => {
             db.query('SELECT * FROM department', (error, result) => {
                 if(!error)
-                resolve(result);
+                    resolve(result);
             })
         })
     }
 
+    //добавление записей в таблицу
     static async addDepartments(departments){
         return new Promise(resolve => {
-            db.query('INSERT INTO department (NAME_DEPARTMENT) VALUES ?', [departments], (error, result) => {
+            const fields = Object.keys(departments[0]).join(',');
+            const values = departments.map(department => Object.values(department));
+
+            db.query(`INSERT INTO department (${fields}) VALUES ?`, [values], (error, result) => {
                 if(!error)
                     resolve(true);
                 else
